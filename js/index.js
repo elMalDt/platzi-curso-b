d1ca0a841820f9b4e6ce3788c031798017de8e61
unknown <miguemix55@hotmail.com>
2018-10-28 14:57:11 -0600

    mi primer commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+/*Aqui la funcion se ejecuta una vez carguen los estilos de la pagina
+y usamos la ayuda de jQuery con uno de los plugins que trae bootstrap*/
+$(function(){
+  $('[data-toggle="tooltip"]').tooltip()
+})
